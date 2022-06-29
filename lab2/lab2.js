d023fb2813ba1b94d99f3d92a1e30d9a4e4edcfe
Mattito <mattquiring02@gmail.com>
2022-06-29 14:38:19 -0700

    second change

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -2,9 +2,13 @@ function greeting(name) {
   console.log(name);
 }
 
+function getDayOfTheWeek() {
+  console.log("The day of the week is Wednesday.");
+}
+
 function greeting2(name) {
   console.log(name);
 }
 
 // Gives permission to use in another file
-module.exports = { greeting, greeting2 };
+module.exports = { greeting, greeting2, getDayOfTheWeek };
--- lab2/main.js
+++ lab2/main.js
@@ -1,3 +1,5 @@
 let functionLogic = require("./lab2.js");
 functionLogic.greeting("some name");
 functionLogic.greeting2("some name");
+
+functionLogic.getDayOfTheWeek();
